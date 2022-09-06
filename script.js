0e7edef5115e9c6d426e3d7a503ce67c5629a7ea
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-09-06 15:40:21 -0300

    finish!

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 $(function () {
     $(".button-collapse").sideNav();
 });
+let mensagemInicial = 'Seja bem-vindo(a) ao meu site portfólio, \nme siga nas redes sociais e github!';
+
+window.alert(mensagemInicial);
